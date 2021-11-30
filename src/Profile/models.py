db7fb0164bbc168d4a78dd499e9df46b6db0d99f
Ben <71015535+vskidevelopers@users.noreply.github.com>
2021-11-30 22:02:01 +0300

    first commit

---
+++ src/Profile/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from .utils import generate_uuid_code
+
+# Create your models here.
+
+#Choices 
+class Year_of_study(models.TextChoices):
+    FIRST='first'
+    SECOND= 'second'
+    THIRD= 'third'
+    FOURTH= 'fourth'
+    FIFTH = 'fifth'
+    SIXTH = 'sixth'
+
+class Study_level(models.TextChoices):
+    CERT ='certificate'
+    DEGREE= 'degree'
+    MASTERS= 'masters'
+    PHD= 'doctrate'
+
+
+class Profiles(models.Model):
+    """Model definition for Profiles."""
+
+    # TODO: Define fields here
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    bio= models.TextField(max_length=500, blank=True, null=True)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    year_of_study=models.CharField(max_length=50, choices=Year_of_study.choices, blank=True, null=True) 
+    course = models.CharField(max_length=200, blank=True, null=True)
+    study_level =models.CharField(max_length=50, choices=Study_level.choices, default=Study_level.CERT) 
+    friends = models.ManyToManyField(User, blank=True, related_name='friends' )
+    avatar = models.ImageField(default="avatar.png",  upload_to="avatars/")
+    slug = models.SlugField(unique=True, blank=True, max_length=500)
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Profiles."""
+
+        verbose_name = 'Profiles'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        """Unicode representation of Profiles."""
+        return f"{self.user.username}-{self.created}"
+
+    def save(self, *args, **kwargs):
+        """Save method for Profiles."""
+        ex =False
+        if self.first_name and self.last_name:
+            slug= slugify(str(self.first_name)+" "+(self.last_name))
+            ex = Profiles.objects.filter(slug=slug).exists()
+            while ex:
+                slug=slugify(slug+" "+ str(generate_uuid_code()))
+                ex=Profiles.objects.filter(slug=slug).exists()
+        else:
+            slug = str(self.user.username)
+        
+        self.slug = slug
+        super().save( *args, **kwargs)
+
